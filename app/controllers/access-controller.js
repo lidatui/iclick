665c5f272929a565467a12013e6f7df21c36a8d5
miemiedev <b31b6b82>
2012-11-01 15:54:30 +0800

    template style

--- app/controllers/access-controller.js
+++ app/controllers/access-controller.js
@@ -13,10 +13,11 @@ module.exports = function(app){
             data = data.replace( /\r|\n/ig , '' ); //去除换行及回车
             data = data.replace( /\/\*[\w\W]*?\*\//ig , ''); //去除注释
             data = data.replace( / {2,}/ig , ' '); //去除多余空格
+            data = data.replace(/\'/g,'\\\''); //转义'
             console.log(data);
             res.writeHead(200, {"Content-Type": "application/x-javascript"});
             var callback = req.query["callback"];
-            var tpl = [ callback, '(\'', data.replace(/\'/g,'\\\''), '\')'];
+            var tpl = [ callback, '(\'', data, '\')'];
             var template = handlebars.compile(tpl.join(''));
             var result = template({name:'miemiedev',highlight:true,title:'哈哈哈',pubDate:'08-15'})
             res.write(result);
