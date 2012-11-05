7398b3417190f88eefd215734c30fbfc8f6f89a7
miemiedev <miemiedev@gmail.com>
2012-11-05 19:47:35 +0800

    gen account

--- app/controllers/index-controller.js
+++ app/controllers/index-controller.js
@@ -15,7 +15,7 @@ module.exports = function(app){
             ,author: 'miemiedev'
         });
     });
-    app.post('/login', function(req, res){
+    app.post('/login', function(req, res,next){
         console.log(req.body);
         User.findOne({loginName: req.body.loginName, loginPwd: req.body.loginPwd}, function(err, user){
             if(user){
@@ -23,8 +23,31 @@ module.exports = function(app){
                 req.session.user = user;
                 res.redirect('/manage/statistics');
             }else{
-                req.session.error = '验证失败，请重新登陆！';
-                res.redirect('login');
+                User.count().exec(function (err, count) {
+                    if(count){
+                        req.session.error = '验证失败，请重新登陆！';
+                        res.redirect('login');
+                    }else{
+                        var user = new User({
+                            loginName: 'admin',
+                            loginPwd: 'admin',
+                            locked: 0,
+                            role:{
+                                article: 1,
+                                accessControl: 1,
+                                template: 1,
+                                statistics: 1,
+                                user: 1
+                            }
+                        });
+                        user.save(function(err){
+                            if(err) return next(err);
+                            req.session.error = '账号已生成，请重新登陆！';
+                            res.redirect('login');
+                        });
+                    }
+                });
+
             }
         });
 
