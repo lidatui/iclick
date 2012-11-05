9087c2e12741c76158f812c776457193219d17af
miemiedev <b31b6b82>
2012-11-05 16:19:08 +0800

    modify url

--- static/js/show.js
+++ static/js/show.js
@@ -3,7 +3,7 @@ var QLZQ = function(){};
 
 QLZQ.head = document.getElementsByTagName('head')[0];
 QLZQ.script = document.createElement("script");
-QLZQ.script.setAttribute("src", 'http://127.0.0.1:3000/show?callback=QLZQ.callback&url='+location.href);
+QLZQ.script.setAttribute("src", 'http://10.29.236.95:3000/show?callback=QLZQ.callback&url='+location.href);
 
 QLZQ.show = function(elementId){
     QLZQ.head.appendChild(QLZQ.script);
