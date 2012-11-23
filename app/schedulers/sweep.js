28b89520a8312f5b0dcec73f2625095ebd450054
miemiedev <b31b6b82>
2012-11-23 16:57:24 +0800

    add schedulers

--- app/schedulers/sweep.js
+++ app/schedulers/sweep.js
@@ -1,4 +1,7 @@
 
 module.exports = function(){
-    console.log('sweep data..');
+    var cronJob = require('cron').CronJob;
+    new cronJob('0 0 0 * * *', function(){
+        console.log('sweep data..');
+    },null,true);
 }
